c44456a84fef8d55e41b2cd35df94dc403b1f5a5
SaleBL <sale.bl@gmail.com>
2016-06-01 19:58:35 +0200

    Cas

---
+++ cas.php
@@ -0,0 +1,33 @@
+<?php
+function writeMsg() {
+    echo "Hello world!";
+}
+$rezultat=writeMsg(); // call the function
+var_dump ($rezultat);
+echo "<hr>";
+
+function faktorijel ($broj) {
+	if ($broj>=1){
+		$rezultat=1;
+		for($i=1; $i<=$broj; $i++)
+			$rezultat*=$i;
+	}
+	return $rezultat;
+}
+$f=faktorijel(5);
+var_dump($f);
+echo "<hr>";
+
+$x=12;
+function sabiranje ($x, $y) {
+	return $x+$y;
+}
+$z=sabiranje(2,5);
+var_dump($z);
+echo "<hr>";
+var_dump($_GET);
+echo "<hr>";
+var_dump($_POST);
+echo "<hr>";
+var_dump($_POST);
+?>
